25e0667b681f51ea6f19c06f31dcc5994981c4d2
rboudrouss <r.boudrouss@gmail.com>
2022-03-29 12:54:12 +0200

    decrypt function

--- src/rsa.c
+++ src/rsa.c
@@ -19,11 +19,16 @@ long *encrypt(char *chaine, long s, long n)
     size_t len = strlen(chaine);
     long *rep = malloc(sizeof(long) * len);
     for (int i = 0; i < len - 1; i++)
-        rep[i] = modpow(chaine[i], s, n);
+        rep[i] = modpow((long)chaine[i], s, n);
     rep[len - 1] = '\0';
     return rep;
 }
 
 char *decrypt(long *crypted, int size, long u, long n)
 {
+    char *rep = malloc(size * sizeof(char));
+    for (int i = 0; i < size - 1; i++)
+        rep[i] = modpow(crypted[i], u, n);
+    rep[size - 1] = '\0';
+    return rep;
 }
