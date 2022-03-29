fe8759d7f9e386e109274ad861cee2aa95a13c00
rboudrouss <r.boudrouss@gmail.com>
2022-03-29 12:56:56 +0200

    test foncitons

--- src/main.c
+++ src/main.c
@@ -2,7 +2,50 @@
 #include "prime.h"
 #include "rsa.h"
 
+void print_long_vector(long *result, int size)
+{
+    printf(" Vector : [ ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%lx \t ", result[i]);
+    }
+    printf("] \n");
+}
+
 int main()
 {
+    srand(time(NULL));
+
+    // Generation de cle :
+    long p = random_prime_number(3, 7, 5000);
+    long q = random_prime_number(3, 7, 5000);
+    while (p == q)
+        q = random_prime_number(3, 7, 5000);
+    long n, s, u;
+    generate_keys_values(p, q, &n, &s, &u);
+    // Pour avoir des cles positives :
+    if (u < 0)
+    {
+        long t = (p - 1) * (q - 1);
+        u = u + t; // on aura toujours s*u mod t = 1
+    }
+
+    // Afichage des cles en hexadecimal
+    printf("cle publique = (%lx , %lx ) \n", s, n);
+    printf("cle privee = (%lx , %lx ) \n", u, n);
+
+    // Chiffrement:
+    char mess[10] = " H ell o ";
+    int len = strlen(mess);
+    long *crypted = encrypt(mess, s, n);
+
+    printf("Initial message : %s \n", mess);
+    printf("Encoded representation : \n");
+    print_long_vector(crypted, len);
+
+    // Dechiffrement
+    char *decoded = decrypt(crypted, len, u, n);
+    printf(" Decoded : %s \n", decoded);
+
     return 0;
 }
