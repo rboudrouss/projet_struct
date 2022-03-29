7e1772644e464f238843d79c1b59da2c700058b6
rboudrouss <r.boudrouss@gmail.com>
2022-03-29 14:46:00 +0200

    yoot

--- crypto/misc.h
+++
@@ -1,8 +0,0 @@
-int sqrti(int n);  //newton's method, returns ceil(sqrt(n))
-int log2_estimate(int n);
-int exp_mod_slow(int n, int exp, int mod);
-int exp_mod(int n, int exp, int mod);
-int is_quad_res(int n, int p); // returns true if n is a quadratic residue mod p using euler criterion. NEEDS p odd AND gcd(n,p)=1
-int gen_ran_int(int low_bound, int up_bound);
-int gcd_extended(int x, int y, int *a, int *b); //returns gcd(a,b)=au+bv (Binary extended cgd algorithm)
-int inv_mod(int n, int m);
--- crypto/primes.h
+++
@@ -1,9 +0,0 @@
-int* erathostene(int n, int* nb_primes); //returns the n first prime numbers using the sieve of erathostenes, and sets *nbrPrimes=pi(n)
-int gen_prime(int low_bound, int up_bound);
-int gen_strong_prime(int low_bound, int up_bound); // Gordon's algorithm
-bool witness(int n, int mant, int exp, int base);
-bool miller_rabin(int n, int nb_base);
-bool miller_rabin_deterministic(int n); // Works for n<2^64
-bool fermat(int n, int nb_test);
-bool naive_primality_base(int n, int *base, int base_size);
-bool naive_primality(int n);
