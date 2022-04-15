8ed968fe00dc8561d0c57b1a5b9afb548f9e39c6
rboudrouss <r.boudrouss@gmail.com>
2022-04-15 16:44:41 +0200

    i forgor again

--- head/block.h
+++ head/block.h
@@ -12,4 +12,13 @@ typedef struct block
     int nonce;
 } Block;
 
+void write_block(char *name);
+Block *read_block(char *name);
+char *block_to_str(Block *block);
+char *hash(char *s, int nonce);
+int first_d_zero(char *s, int d);
+void compute_proof_of_work(Block *b, int d);
+int verify_block(Block *b, int d);
+void delete_block(Block* b);
+
 #endif
